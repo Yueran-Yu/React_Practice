1d0fd2025fa912820f0d00b48dd4f9aac686c937
GraceYu <librayuyueran@gmail.com>
2021-03-14 11:13:57 -0500

    create card folder and single component of a card, add the style

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -3,8 +3,8 @@ import { CardList } from './components/card-list/card-list.component.jsx'
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       monsters: []
     }

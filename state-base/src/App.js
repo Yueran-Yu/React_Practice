82099834d4482f08a39c1817f08f0ec99b2a4e30
GraceYu <librayuyueran@gmail.com>
2021-03-20 20:58:41 -0500

    using function to update the status instead of an object

--- state-base/src/App.js
+++ state-base/src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      meaningOfLife: 47
+    }
+  }
+  // use arrow function so there will be no issue with 'this' keyword
+  handleClick = () => {
+    // first parameter: a function, second parameter: a callback function
+    // anytime you want to update state, you should use a function instead of an object
+    this.setState((prevState, prevProps) => ({meaningOfLife: prevState.meaningOfLife + 1}), () => console.log(this.state.meaningOfLife))
+  }
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p>{this.state.meaningOfLife}
+            </p>
+            <button onClick={this.handleClick}>
+              Update State
+            </button>
+          </header>
+        </div>
+    );
+  }
 }
-
 export default App;
